fdf6a46f856a60941a37dc9d7ef41e79fc1bac4d
Rideboard Project <99418443+INCS870@users.noreply.github.com>
2022-04-03 02:47:50 -0700

    editable profile details

---
+++ RideboardWeb/src/main/webapp/resources/scripts/jquery.validate.date.js
@@ -0,0 +1,4 @@
+﻿$.validator.methods.date = function (value, element) {
+    var dateRegex = /^(0?[1-9]|[12]\d|3[01])\/(0?[1-9]|10|11|12)\/(19|20)\d\d(\D.*)?$/;
+    return this.optional(element) || dateRegex.test(value);
+};
